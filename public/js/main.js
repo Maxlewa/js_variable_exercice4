ab96603b01d9ac151bb33d91838d3273a940a3bc
Maxence <lemairemaxence@hotmail.com>
2020-12-28 15:49:51 +0100

    exo 4 ok

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+// 1
+
+// let nom = "Lemaire"
+// let prenom = "Maxence"
+// let ville = prompt("Ville ?")
+// alert(ville)
+// console.log(nom + " " + prenom);
+
+// 2A
+
+// let nom = "Lemaire"
+// let prenom = "Maxence"
+// let villDepart = "Bruxelles"
+// let villDestination = "Montréal"
+
+// console.log("Le voyage de " + villDepart + " vers " + villDestination + " a été enregistré au nom de " + nom + " " + prenom);
+// alert("Le voyage de " + villDepart + " vers " + villDestination + " a été enregistré au nom de " + nom + " " + prenom);
+
+// 2B
+
+let nom = prompt('Votre nom :')
+let prenom = prompt('Votre prénom :')
+let villDepart = prompt('Ville de départ ?')
+let villDestination = prompt("Ville d'arrivée ?")
+
+alert("Le voyage de " + villDepart + " vers " + villDestination + " a été enregistré au nom de " + nom + " " + prenom);
